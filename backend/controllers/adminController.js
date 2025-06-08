fc2851ba6967a7f7f2ea11c153b7d95bad038374
Adithya Shaji <adithyas038@gmail.com>
2025-06-08 22:47:06 +0530

    Established connection with database

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,10 +2,10 @@
 
 
 //API for adding doctor
-const addDoctor = async (req,res){
+const addDoctor = async (req,res)=>{
   try{
     const{name,email,password,speciality,degree,experience,about,fees,address} = req.body
-    
+
   } catch (error){
 
   }
